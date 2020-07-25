86e322ffbe2a1b340abe99e3f8ea34b8b4ba4292
Christopher Potti <christopherpotti@PSADMINs-Air.lan>
2020-07-25 16:04:07 -0400

    added icons

--- blue-bell/src/components/Header.js
+++ blue-bell/src/components/Header.js
@@ -4,9 +4,9 @@ class Header extends Component {
     render() {
         return (
             <div className='container1'>
-                <a className='bell' src='##'></a>
+                <a className='bell' src='##'><i class="fa fa-bell" aria-hidden="true"></i></a>
                 <h1 className='title1'><a> BlueBell </a></h1>
-                <a className='settings' src='##'></a>
+                <a className='settings' src='##'><i class="fa fa-cogs" aria-hidden="true"></i></a>
             </div>
         );
     }
