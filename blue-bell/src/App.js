d0d52b1a31bc6639c7f43b56de2714aeae684a64
Christopher Potti <christopherpotti@PSADMINs-Air.lan>
2020-07-25 15:42:24 -0400

    nav area is linked to the app.js component

--- blue-bell/src/App.js
+++ blue-bell/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Userinfo from './components/Userinfo';
 import Contentarea from './components/Contentarea';
+import Navarea from './components/Navarea';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Userinfo/>
       <Contentarea/>
+      <Navarea/>
     </div>
   );
 }
