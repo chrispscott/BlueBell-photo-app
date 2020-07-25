929de39f8e7db4a991b2a33c3cfb017fc40e0667
Christopher Potti <christopherpotti@PSADMINs-Air.lan>
2020-07-25 15:30:49 -0400

    linked the header component to app.js

--- blue-bell/src/components/Header.js
+++ blue-bell/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                
+                <h1>This is the header Component</h1>
             </div>
         );
     }
