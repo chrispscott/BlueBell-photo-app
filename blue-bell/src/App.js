c2ad4432737af34a6e8d9b9a5ef34f23c81a9a4c
Christopher Potti <christopherpotti@PSADMINs-Air.lan>
2020-07-25 15:36:13 -0400

    the user info component is linked to app.js

--- blue-bell/src/App.js
+++ blue-bell/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Userinfo from './components/Userinfo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Userinfo/>
     </div>
   );
 }
