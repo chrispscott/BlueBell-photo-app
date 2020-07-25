0ef8f2aeb9663dee087536b724295dfb65eaee7a
Christopher Potti <christopherpotti@PSADMINs-Air.lan>
2020-07-25 15:39:04 -0400

    content area is linked to app.js

--- blue-bell/src/App.js
+++ blue-bell/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Userinfo from './components/Userinfo';
+import Contentarea from './components/Contentarea';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Userinfo/>
+      <Contentarea/>
     </div>
   );
 }
